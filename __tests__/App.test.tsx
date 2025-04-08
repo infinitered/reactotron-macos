07885da6734977bd29829300339ce89432a44de8
Jamon Holmgren <code@jamon.dev>
2025-04-08 12:36:15 -0700

    Initialize new react-native-macos@0.78.3

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+
+// import React from 'react';
+// import App from '../App';
+
+test("renders correctly", async () => {
+  // TODO: Figure out best way to test desktop
+  expect(true).toBe(true)
+})
