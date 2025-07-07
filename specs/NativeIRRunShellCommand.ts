6c867ee5ea2808a8d23b3ca6fff765871f5de4ef
Leon Kim <minusnine@gmail.com>
2025-07-08 06:20:50 +0900

    runTaskWithCommand spec with event listeners

--- specs/NativeIRRunShellCommand.ts
+++ specs/NativeIRRunShellCommand.ts
@@ -1,10 +1,25 @@
+import type { EventEmitter } from "react-native/Libraries/Types/CodegenTypes"
 import type { TurboModule } from "react-native"
 import { TurboModuleRegistry } from "react-native"
+
+export interface ShellCommandOutputEvent {
+  taskId: string
+  output: string
+  type: "stdout" | "stderr"
+}
+
+export interface ShellCommandCompleteEvent {
+  taskId: string
+  exitCode: number
+}
 
 export interface Spec extends TurboModule {
   runAsync(command: string): Promise<string>
   runSync(command: string): string
   runCommandOnShutdown(command: string): void
+  runTaskWithCommand(command: string, args: ReadonlyArray<string>, taskId: string): void
+  readonly onShellCommandOutput: EventEmitter<ShellCommandOutputEvent>
+  readonly onShellCommandComplete: EventEmitter<ShellCommandCompleteEvent>
 }
 
 export default TurboModuleRegistry.getEnforcing<Spec>("IRRunShellCommand")
