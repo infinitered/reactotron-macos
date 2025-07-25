2884f10262598074601728d489d16fbc3e758d1b
Leon Kim <minusnine@gmail.com>
2025-07-26 05:00:08 +0900

    add android native

---
+++ android/app/.cxx/Debug/5iz6ks71/prefab/armeabi-v7a/prefab/lib/arm-linux-androideabi/cmake/hermes-engine/hermes-engineConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET hermes-engine::libhermes)
+add_library(hermes-engine::libhermes SHARED IMPORTED)
+set_target_properties(hermes-engine::libhermes PROPERTIES
+    IMPORTED_LOCATION "/Users/blackgoat/.gradle/caches/8.12/transforms/a5fe47236b5401853ffdb5a60bc2dbbc/transformed/hermes-android-0.78.3-debug/prefab/modules/libhermes/libs/android.armeabi-v7a/libhermes.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/blackgoat/.gradle/caches/8.12/transforms/a5fe47236b5401853ffdb5a60bc2dbbc/transformed/hermes-android-0.78.3-debug/prefab/modules/libhermes/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
