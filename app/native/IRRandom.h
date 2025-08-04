245f8564976b7048c4d03ff72f1f0850f01a8af7
Jamon Holmgren <code@jamon.dev>
2025-08-04 16:59:53 -0700

    whatever asdf

---
+++ app/native/IRRandom.h
@@ -0,0 +1,10 @@
+#import <Foundation/Foundation.h>
+#import <AppSpec/AppSpec.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface IRRandom : NativeIRRandomSpecBase <NativeIRRandomSpec>
+
+@end
+
+NS_ASSUME_NONNULL_END
