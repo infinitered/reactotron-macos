0853449ba15a28787bf34265c3f3cd8e8f85b41d
Sean Barker <theseanbarker@gmail.com>
2025-09-04 12:39:17 -0400

    Comment out tabs for now

--- app/components/Tabs.tsx
+++ app/components/Tabs.tsx
@@ -1,31 +1,33 @@
 import { Text, View, type ViewStyle } from "react-native"
-import IRTabComponentView from "../native/IRTabComponentView/NativeIRTabNativeComponent"
+// import IRTabComponentView from "../native/IRTabComponentView/NativeIRTabNativeComponent"
 import { themed } from "../theme/theme"
 import { $flex } from "../theme/basics"
 
+
 export function Tabs() {
-  return (
-    <IRTabComponentView
-      style={$flex}
-      tabs={[
-        { id: "home", title: "Home" },
-        { id: "settings", title: "Settings" },
-      ]}
-      onTabSelected={(event) => {
-        console.log("selectedTabId", event.nativeEvent.selectedTabId)
-      }}
-    >
-      <View style={$panel()} key="home" id="home">
-        <Text>Home</Text>
-      </View>
-      <View style={$panel()} key="settings" id="settings">
-        <Text>Settings</Text>
-      </View>
-    </IRTabComponentView>
-  )
+  return null
+  // return (
+  //   <IRTabComponentView
+  //     style={$flex}
+  //     tabs={[
+  //       { id: "home", title: "Home" },
+  //       { id: "settings", title: "Settings" },
+  //     ]}
+  //     onTabSelected={(event) => {
+  //       console.log("selectedTabId", event.nativeEvent.selectedTabId)
+  //     }}
+  //   >
+  //     <View style={$panel()} key="home" id="home">
+  //       <Text>Home</Text>
+  //     </View>
+  //     <View style={$panel()} key="settings" id="settings">
+  //       <Text>Settings</Text>
+  //     </View>
+  //   </IRTabComponentView>
+  // )
 }
 
-const $panel = themed<ViewStyle>(({}) => ({
+const $panel = themed<ViewStyle>(({ }) => ({
   flexDirection: "row",
   alignItems: "center",
   justifyContent: "center",
