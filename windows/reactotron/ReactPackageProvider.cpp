7b74137fc0b9abbde92961ed41ca2e3aff366bab
Sean Barker <theseanbarker@gmail.com>
2025-09-05 16:12:57 -0400

    Add module files

--- windows/reactotron/ReactPackageProvider.cpp
+++ windows/reactotron/ReactPackageProvider.cpp
@@ -2,6 +2,8 @@
 #include "ReactPackageProvider.h"
 #include "NativeModules.h"
 
+#include "IRNativeModules.g.h"
+
 using namespace winrt::Microsoft::ReactNative;
 
 namespace winrt::reactotron::implementation
@@ -10,6 +12,7 @@ namespace winrt::reactotron::implementation
     void ReactPackageProvider::CreatePackage(IReactPackageBuilder const &packageBuilder) noexcept
     {
         AddAttributedModules(packageBuilder, true);
+        RegisterAllFabricComponents(packageBuilder);
     }
 
 } // namespace winrt::reactotron::implementation
