4bd0d911bb53449d7e8318193f884f54830520bf
Sean Barker <theseanbarker@gmail.com>
2025-09-09 11:03:15 -0400

    implement clipboard functionality

--- app/native/IRClipboard/IRClipboard.windows.cpp
+++ app/native/IRClipboard/IRClipboard.windows.cpp
@@ -17,14 +17,65 @@ namespace winrt::reactotron::implementation
 
     std::string IRClipboard::getString() noexcept
     {
-        // TODO: Implement clipboard get functionality for Windows
-        // Use GetClipboardData with CF_TEXT format
-        return "";
+        if (!OpenClipboard(nullptr))
+        {
+            return "";
+        }
+
+        HANDLE hData = GetClipboardData(CF_TEXT);
+        if (hData == nullptr)
+        {
+            CloseClipboard();
+            return "";
+        }
+
+        char* pszText = static_cast<char*>(GlobalLock(hData));
+        if (pszText == nullptr)
+        {
+            CloseClipboard();
+            return "";
+        }
+
+        std::string text(pszText);
+        GlobalUnlock(hData);
+        CloseClipboard();
+
+        return text;
     }
 
     void IRClipboard::setString(std::string text) noexcept
     {
-        // TODO: Implement clipboard set functionality for Windows
-        // Use SetClipboardData with CF_TEXT format
+        if (!OpenClipboard(nullptr))
+        {
+            return;
+        }
+
+        EmptyClipboard();
+
+        size_t textLength = text.length() + 1;
+        HGLOBAL hClipboardData = GlobalAlloc(GMEM_DDESHARE, textLength);
+        if (hClipboardData == nullptr)
+        {
+            CloseClipboard();
+            return;
+        }
+
+        char* pchData = static_cast<char*>(GlobalLock(hClipboardData));
+        if (pchData == nullptr)
+        {
+            GlobalFree(hClipboardData);
+            CloseClipboard();
+            return;
+        }
+
+        strcpy_s(pchData, textLength, text.c_str());
+        GlobalUnlock(hClipboardData);
+
+        if (SetClipboardData(CF_TEXT, hClipboardData) == nullptr)
+        {
+            GlobalFree(hClipboardData);
+        }
+
+        CloseClipboard();
     }
 }
